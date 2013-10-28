81609087bcc7e65cd1b8b9043b988c606453db10
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2013-10-28 19:01:25 +0100

    Remove install check for ufo

--- setup.py
+++ setup.py
@@ -1,13 +1,6 @@
-import pkgconfig
 from setuptools import setup, find_packages
 
 VERSION='0.2dev'
-
-REQUIRED_UFO='>=0.5'
-
-if not pkgconfig.installed('ufo', REQUIRED_UFO):
-    print("You need at least ufo-core {0}. The installed scripts "
-          "might not work as expected.\n".format(REQUIRED_UFO))
 
 setup(
     name='ufo-scripts',
