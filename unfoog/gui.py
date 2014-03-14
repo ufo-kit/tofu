f6f77561416e1cd5e3622435f68afd7bb454527e
Maria Matveeva <matveeva.maria@gmail.com>
2014-03-14 14:45:44 +0100

    Enhance GUI
    Added imagej viewer, logger window and correction.

--- unfoog/gui.py
+++ unfoog/gui.py
@@ -1,7 +1,23 @@
 import sys
+import os
 import time
+import logging
+import tempfile
+import threading
+import subprocess
 from PyQt4 import QtGui, QtCore
 from . import reco, config
+
+
+LOG = logging.getLogger(__name__)
+log = tempfile.NamedTemporaryFile(delete = False, suffix = '.txt')
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s:%(levelname)s:%(name)s:%(message)s',
+    filename=log.name,
+    filemode='a'
+)
 
 
 def _set_line_edit_to_path(line_edit):
@@ -34,11 +50,12 @@ class ApplicationWindow(QtGui.QMainWindow):
         QtGui.QMainWindow.__init__(self)
         self.params = params
         self.app = app
+        self.imagej_available = any([os.path.exists(os.path.join(p,"imagej")) for p in os.environ["PATH"].split(os.pathsep)])
         self.do_layout()
 
     def do_layout(self):
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
-        self.setWindowTitle("application main window")
+        self.setWindowTitle("Reconstruction")
 
         file_menu = QtGui.QMenu('&File', self)
         file_menu.addAction('&Quit', self.on_close,
@@ -48,21 +65,24 @@ class ApplicationWindow(QtGui.QMainWindow):
         self.main_widget = QtGui.QWidget(self)
         main_vbox = QtGui.QVBoxLayout()
 
+        self.setGeometry(300, 200, 200, 200)
+        self.resize(575, 520)
+
         # Input group
         input_group = QtGui.QGroupBox("Input")
         input_grid = QtGui.QGridLayout()
         input_group.setLayout(input_grid)
         input_group.setFlat(True)
 
-        sino_button = QtGui.QRadioButton("Sinograms")
+        self.sino_button = QtGui.QRadioButton("Sinograms")
         self.proj_button = QtGui.QRadioButton("Projections")
         input_path_button = QtGui.QPushButton("Browse ...")
         self.input_path_line = _new_path_line_edit(self.params.input)
 
-        sino_button.setChecked(not self.params.from_projections)
-        self.proj_button.setChecked(self.params.from_projections is not None)
+        self.sino_button.setChecked(True)
+        self.proj_button.setChecked(False)
 
-        input_grid.addWidget(sino_button, 0, 0)
+        input_grid.addWidget(self.sino_button, 0, 0)
         input_grid.addWidget(self.proj_button, 1, 0)
         input_grid.addWidget(self.input_path_line, 2, 0)
         input_grid.addWidget(input_path_button, 2, 1)
@@ -95,10 +115,42 @@ class ApplicationWindow(QtGui.QMainWindow):
 
         self.output_path_line = _new_path_line_edit(self.params.output)
         output_path_button = QtGui.QPushButton("Browse ...")
+        self.imagej_checkbox = QtGui.QCheckBox("Show Images after Reconstruction", self)
+
+        if self.imagej_available == False:
+            self.imagej_checkbox.setEnabled(False)
+            LOG.debug("ImageJ is not installed")
 
         output_grid.addWidget(QtGui.QLabel("Path:"), 0, 0)
         output_grid.addWidget(self.output_path_line, 0, 1)
         output_grid.addWidget(output_path_button, 0, 2)
+        output_grid.addWidget(self.imagej_checkbox, 1, 0)
+
+        # Darks & Flats Group
+        correction_grid = QtGui.QGridLayout()
+        self.correction_group = QtGui.QGroupBox("Correction for Projections")
+        self.correction_group.setLayout(correction_grid)
+        self.correction_group.setFlat(True)
+
+        self.correct_box = QtGui.QCheckBox("Use Correction", self)
+        self.darks_path_line = QtGui.QLineEdit()
+        self.darks_path_button = QtGui.QPushButton("Browse...")
+        self.darks_label = QtGui.QLabel("Dark-field:")
+        self.flats_path_line = QtGui.QLineEdit()
+        self.flats_path_button = QtGui.QPushButton("Browse...")
+        self.flats_label = QtGui.QLabel("Flat-field:")
+
+        correction_grid.addWidget(self.correct_box, 0, 0)
+        correction_grid.addWidget(self.darks_label, 1, 0)
+        correction_grid.addWidget(self.darks_path_line, 1, 1)
+        correction_grid.addWidget(self.darks_path_button, 1, 2)
+        correction_grid.addWidget(self.flats_label, 2, 0)
+        correction_grid.addWidget(self.flats_path_line, 2, 1)
+        correction_grid.addWidget(self.flats_path_button, 2, 2)
+
+        self.correct_box.setEnabled(False)
+        self.status = False
+        self.change_status()
 
         # Button group
         button_group = QtGui.QDialogButtonBox()
@@ -107,11 +159,15 @@ class ApplicationWindow(QtGui.QMainWindow):
         reco_button = button_group.addButton("Reconstruct", QtGui.QDialogButtonBox.AcceptRole)
         save_button = button_group.addButton("Save", QtGui.QDialogButtonBox.AcceptRole)
 
+        # Log widget
+        self.textWidget = QtGui.QTextBrowser(self)
+
         main_vbox.addWidget(input_group)
         main_vbox.addWidget(param_group)
         main_vbox.addWidget(output_group)
-        main_vbox.addStretch()
+        main_vbox.addWidget(self.correction_group)
         main_vbox.addWidget(button_group)
+        main_vbox.addWidget(self.textWidget)
 
         # Connect things
         input_path_button.clicked.connect(self.on_input_path_clicked)
@@ -119,19 +175,73 @@ class ApplicationWindow(QtGui.QMainWindow):
         close_button.clicked.connect(self.on_close)
         reco_button.clicked.connect(self.on_reconstruct)
         save_button.clicked.connect(self.on_save)
+        self.proj_button.clicked.connect(self.on_proj_button_clicked)
+        self.sino_button.clicked.connect(self.on_sino_button_clicked)
+        self.correct_box.clicked.connect(self.on_correct_box_clicked)
+        self.connect(self, QtCore.SIGNAL('triggered()'), self.closeEvent)
+        self.darks_path_button.clicked.connect(self.on_darks_path_clicked)
+        self.flats_path_button.clicked.connect(self.on_flats_path_clicked)
 
         self.main_widget.setLayout(main_vbox)
         self.main_widget.setFocus()
         self.setCentralWidget(self.main_widget)
 
+    def on_correct_box_clicked(self, checked):
+        if self.correct_box.isChecked():
+            self.status = True
+            self.change_status()
+        else:
+            self.status = False
+            self.change_status()
+
+    def change_status(self):
+        self.darks_path_line.setEnabled(self.status)
+        self.darks_path_button.setEnabled(self.status)
+        self.darks_label.setEnabled(self.status)
+        self.flats_path_line.setEnabled(self.status)
+        self.flats_path_button.setEnabled(self.status)
+        self.flats_label.setEnabled(self.status)
+
+    def on_proj_button_clicked(self, checked):
+        if self.proj_button.isChecked():
+            self.correct_box.setEnabled(True)
+
+    def on_sino_button_clicked(self, checked):
+        if self.sino_button.isChecked():
+            self.correct_box.setEnabled(False)
+            self.status = False
+            self.change_status()
+
     def on_input_path_clicked(self, checked):
         _set_line_edit_to_path(self.input_path_line)
+        if "sinogram" in str(self.input_path_line.text()):
+            self.sino_button.setChecked(True)
+            self.proj_button.setChecked(False)
+            self.correct_box.setEnabled(False)
+            self.status = False
+            self.change_status()
+        elif "projection" in str(self.input_path_line.text()):
+            self.sino_button.setChecked(False)
+            self.proj_button.setChecked(True)
+            self.correct_box.setEnabled(True)
 
     def on_output_path_clicked(self, checked):
         _set_line_edit_to_path(self.output_path_line)
 
+    def on_darks_path_clicked(self, checked):
+        _set_line_edit_to_path(self.darks_path_line)
+
+    def on_flats_path_clicked(self, checked):
+        _set_line_edit_to_path(self.flats_path_line)
+
     def on_close(self):
         self.close()
+
+    def closeEvent(self, event):
+        try:
+            os.remove(log.name)
+        except OSError as e:
+            pass
 
     def on_save(self):
         d = self.param_dict()
@@ -150,14 +260,42 @@ class ApplicationWindow(QtGui.QMainWindow):
         self.params.output = str(self.output_path_line.text())
         self.params.from_projections = self.proj_button.isChecked()
 
+        if self.correct_box.isChecked():
+            self.params.darks = str(self.darks_path_line.text())
+            self.params.flats = str(self.flats_path_line.text())
+        else:
+            self.params.darks = None
+            self.params.flats = None
+
         try:
             reco.tomo(self.params)
 
         except Exception as e:
             QtGui.QMessageBox.warning(self, "Warning", str(e))
 
+        if self.imagej_checkbox.isChecked():
+
+            output_path = str(self.output_path_line.text())
+            if output_path == ".":
+                output_path = str(os.getcwd())
+
+            tif = tempfile.NamedTemporaryFile(delete = False, suffix = '.ijm')
+            tif.write('run("Image Sequence...", "open=')
+            tif.write(output_path)
+            tif.write(' number=-1 starting=1 increment=1 scale=100 file=[] sort use");')
+            tif.seek(0)
+
+            def call_imagej():
+                subprocess.call(["imagej -macro " + tif.name], shell = True)
+
+            process = threading.Thread(target = call_imagej)
+            process.start()
+
         _disable_wait_cursor()
         self.main_widget.setEnabled(True)
+        log.seek(0)
+        logtxt = open(log.name).read()
+        self.textWidget.setPlainText(logtxt)
 
 
 def main(params):
