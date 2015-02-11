42c175744ba5ad2e1648ceaa737414fea8fbb474
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2015-02-11 09:39:58 +0100

    Bring back range_from still used by ufo-perf
    @MariaMatveeva: please avoid removing functionality in case it is not clear if
    it's still in use.

--- unfoog/util.py
+++ unfoog/util.py
@@ -5,9 +5,35 @@ import logging
 import os
 
 
+def range_from(s):
+    """
+    Split *s* separated by ':' into int triple, filling missing values with 1s.
+    """
+    def check(region):
+        if region[0] >= region[1]:
+            raise ValueError("'From' must be less than 'to'")
+
+    lst = [int(x) for x in s.split(':')]
+
+    if len(lst) == 1:
+        frm = lst[0]
+        return (frm, frm + 1, 1)
+
+    if len(lst) == 2:
+        check(lst)
+        return (lst[0], lst[1], 1)
+
+    if len(lst) == 3:
+        check(lst)
+        return (lst[0], lst[1], lst[2])
+
+    raise ValueError("Cannot parse {}".format(s))
+
+
 def set_reader(reader, input_prefix, region=None):
     """Set up a *reader* to read from *input_prefix* and use *region*."""
     reader.props.path = input_prefix
+
 
 def check_input(input_prefix, region):
     """Check if there are enough file from *input_prefix* to satisfy *region*."""
@@ -15,6 +41,7 @@ def check_input(input_prefix, region):
     region = range_from(region)
     if total - region[0] < len(range(*region)):
         raise ValueError('Not enough files to satisfy region')
+
 
 def positive_int(value):
     """Convert *value* to an integer and make sure it is positive."""
