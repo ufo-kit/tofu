ff9132e22bc34cb0da21a001abc15a42ebde742c
elisa <elisa@elisa-debian.gateway.sasktel.net>
2019-07-24 11:17:30 -0600

    Restore previous changes:  - add bh3 filter option  - add setting butterworth filter parameters

--- tofu/config.py
+++ tofu/config.py
@@ -226,7 +226,15 @@ SECTIONS['reconstruction'] = {
         'default': 'ramp-fromreal',
         'type': str,
         'help': "Projection filter",
-        'choices': ['ramp', 'ramp-fromreal', 'butterworth', 'faris-byer']},
+        'choices': ['ramp', 'ramp-fromreal', 'butterworth', 'faris-byer', 'bh3']},
+    'bworder': {
+        'default': 4.0,
+        'type': float,
+        'help': "Order of the Butterworth filter"},
+    'cutoff': {
+        'default': 0.5,
+        'type': float,
+        'help': "Relative cutoff frequency"},
     'projection-padding-mode': {
         'choices': ['none', 'clamp', 'clamp_to_edge', 'repeat'],
         'default': 'clamp_to_edge',
