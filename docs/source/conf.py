88ebc2a39138f2243960e5bdd3e9eb9567b17263
Tomas Farago <sensej007@email.cz>
2021-12-21 15:31:11 +0100

    docs: don't crash on missing gi

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,6 +50,8 @@ extensions = [
     'sphinx.ext.githubpages',
 ]
 
+autodoc_mock_imports = ['gi']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
