c80b24704b5bcf154513ab493e369ec55bf12acc
Tomas Farago <sensej007@email.cz>
2022-07-15 09:47:10 +0200

    Make docs broader on the page

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -86,6 +86,7 @@ pygments_style = None
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_style = 'css/custom.css'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
