7674099ec913a1b233efd7d95eba7d7c817f1f99
Tomas Farago <sensej007@email.cz>
2024-05-24 13:52:26 +0200

    util: Use TiffFileSequenceReader

--- tofu/util.py
+++ tofu/util.py
@@ -160,13 +160,21 @@ def next_power_of_two(number):
     return 2 ** int(math.ceil(math.log(number, 2)))
 
 
-def read_image(filename):
-    """Read image from file *filename*."""
-    if filename.lower().endswith('.tif') or filename.lower().endswith('.tiff'):
-        from tifffile import TiffFile
-        with TiffFile(filename) as tif:
-            return tif.asarray(out='memmap')
-    elif '.edf' in filename.lower():
+def read_image(filename, allow_multi=False):
+    """Read image from file *filename*. In case of tif files, *filename* can be a regular expression
+    matching more files. If *allow_multi* is True and there are more images in the *filename*,
+    return them all, not only the first one.
+    """
+    if os.path.isdir(filename):
+        format_check = glob.glob(os.path.join(filename, "*"))[0]
+    else:
+        format_check = filename
+
+    if format_check.lower().endswith('.tif') or format_check.lower().endswith('.tiff'):
+        reader = TiffSequenceReader(filename)
+        images = [reader.read(i) for i in range(reader.num_images)]
+        return images if allow_multi else images[0]
+    elif '.edf' in format_check.lower():
         import fabio
         edf = fabio.edfimage.edfimage()
         edf.read(filename)
@@ -398,3 +406,108 @@ class Vector(object):
 
     def __str__(self):
         return repr(self)
+
+
+class FileSequenceReader:
+
+    """Image sequence reader optimized for reading consecutive images. One multi-page image file is
+    not closed after an image is read so that it does not have to be re-opened for reading the next
+    image. The :func:`.close` function must be called explicitly in order to close the last opened
+    image.
+    """
+
+    def __init__(self, file_prefix, ext=''):
+        if os.path.isdir(file_prefix):
+            file_prefix = os.path.join(file_prefix, '*' + ext)
+        self._filenames = sorted(glob.glob(file_prefix))
+        if not self._filenames:
+            raise SequenceReaderError("No files matching `{}' found".format(file_prefix))
+        self._lengths = {}
+        self._file = None
+        self._filename = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+
+    @property
+    def num_images(self):
+        num = 0
+        for filename in self._filenames:
+            num += self._get_num_images_in_file(filename)
+
+        return num
+
+    def read(self, index):
+        if index < 0:
+            # Enables negative indexing
+            index += self.num_images
+        file_index = 0
+        while index >= 0:
+            if file_index >= len(self._filenames):
+                raise SequenceReaderError('image index greater than sequence length')
+            index -= self._get_num_images_in_file(self._filenames[file_index])
+            file_index += 1
+
+        file_index -= 1
+        index += self._lengths[self._filenames[file_index]]
+        self._open(self._filenames[file_index])
+
+        return self._read_real(index)
+
+    def _open(self, filename):
+        if self._filename != filename:
+            if self._filename:
+                self.close()
+            self._file = self._open_real(filename)
+            self._filename = filename
+
+    def close(self):
+        if self._filename:
+            self._close_real()
+            self._file = None
+            self._filename = None
+
+    def _get_num_images_in_file(self, filename):
+        if filename not in self._lengths:
+            self._open(filename)
+            self._lengths[filename] = self._get_num_images_in_file_real()
+
+        return self._lengths[filename]
+
+    def _open_real(self, filename):
+        """Returns an open file."""
+        raise NotImplementedError
+
+    def _close_real(self, filename):
+        raise NotImplementedError
+
+    def _get_num_images_in_file_real(self):
+        raise NotImplementedError
+
+    def _read_real(self, index):
+        raise NotImplementedError
+
+
+class TiffSequenceReader(FileSequenceReader):
+    def __init__(self, file_prefix, ext='.tif'):
+        super(TiffSequenceReader, self).__init__(file_prefix, ext=ext)
+
+    def _open_real(self, filename):
+        import tifffile
+        return tifffile.TiffFile(filename)
+
+    def _close_real(self):
+        self._file.close()
+
+    def _get_num_images_in_file_real(self):
+        return len(self._file.pages)
+
+    def _read_real(self, index):
+        return self._file.pages[index].asarray()
+
+
+class SequenceReaderError(Exception):
+    pass
